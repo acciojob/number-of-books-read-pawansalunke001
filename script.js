761f253a91895ff427523be51f8d0053160c6ccf
Pawan Salunke <133101693+pawansalunke001@users.noreply.github.com>
2023-08-23 16:09:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
+	 return library.reduce((count, book) => count + (book.readingStatus ? 1 : 0), 0);
 };
 
 // Do not change the code below
